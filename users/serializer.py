eee3e8bf302ac4bc3332ee1b63f85a1f7bab30af
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-03-07 09:19:26 +0600

    create a 'login' serializer

--- users/serializer.py
+++ users/serializer.py
@@ -36,3 +36,23 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop('password_check', '')
         return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def validate(self, data):
+        username = data.get('username', '')
+        password = data.get('password', '')
+        user = authenticate(username=username, password=password)
+        data['user'] = user
+
+        if not user:
+            raise serializers.ValidationError('Invalid credentials, try again')
+
+        return data
