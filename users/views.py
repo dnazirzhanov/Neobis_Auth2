ec795dee1fea3a0bf8d1b40249e9793e555c8d4b
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-03-07 09:21:32 +0600

    create a 'register' and 'login' functionality

--- users/views.py
+++ users/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from random import choices
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serializers import (
+    RegisterSerializer,
+    LoginSerializer,
+    ProfileSerializer,
+    PhoneNumberSerializer,
+    CodePhoneNumberSerializer,
+)
+from .models import PersonalData, PhoneNumber
 
-# Create your views here.
+
+class RegisterView(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            return Response({"user": serializer.data, "access_token": access_token}, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.validated_data['user']
+            refresh = RefreshToken.for_user(user)
+            return Response({'username': user.username, 'refresh': str(refresh),
+                             'access': str(refresh.access_token)}, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
