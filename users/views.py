1717f801c9c47ece727c21e8aa232d404bfb1ad1
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-03-07 09:23:25 +0600

    create a 'add phone number' and 'activate a phone number' functionality

--- users/views.py
+++ users/views.py
@@ -81,3 +81,63 @@ class ProfileAPIView(generics.GenericAPIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AddPhoneNumberAPIView(generics.GenericAPIView):
+    serializer_class = PhoneNumberSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request):
+        user = request.user
+        request.data['user'] = user.id
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            phone_number = serializer.save()
+            # code_activation = ''.join(choices('0123456789', k=4))
+            code_activation = '1234'
+            phone_number.code_activation = code_activation
+            phone_number.save()
+            # phone_number = personal_data.phone_number
+            # send_verification_code(phone_number, code_activation)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        user = request.user
+        phone_number = PhoneNumber.objects.get(user_id=user.id)
+        serializer = self.serializer_class(phone_number)
+        return Response(serializer.data)
+
+    def put(self, request):
+        user = request.user
+        request.data['user'] = user.id
+        phone_number = PhoneNumber.objects.get(user_id=user.id)
+        serializer = self.serializer_class(phone_number, data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ActivatePhoneNumberAPIView(generics.GenericAPIView):
+    serializer_class = CodePhoneNumberSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request):
+        user = request.user
+        code_activation_request = request.data.get('code_activation')
+        code_activation_user = PhoneNumber.objects.get(user_id=user.id).code_activation
+        if code_activation_request == code_activation_user:
+            user.is_verified = True
+            user.save()
+            return Response(
+                {'message': 'Phone number verified successfully.'}, status=status.HTTP_200_OK
+            )
+        else:
+            return Response(
+                {'message': 'Please enter the correct verification code.'}, status=status.HTTP_400_BAD_REQUEST
+            )
