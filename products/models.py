ef442c74cce12026893c9e17e904bfd0883c8eed
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-03-08 10:52:23 +0600

    Create a 'Products' Model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Product(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=100)
+    product_price = models.PositiveIntegerField()
+    product_photo = models.ImageField(upload_to='photos/', null=True, blank=True)
+    short_description = models.CharField(max_length=150)
+    detailed_description = models.TextField()
+    is_favorite = models.BooleanField(default=False)
+    favorite_count = models.PositiveIntegerField(default=0)
+    favorites = models.ManyToManyField(User, related_name='favorite_products', blank=True)
+
+    def __str__(self):
+        return f"id: {self.pk} - user_id: {self.username_id} - title: {self.product_name}"
