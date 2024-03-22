699a84fe6108ba44291731a59a4b2e19b60011aa
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-03-22 12:13:50 +0600

    add cloudinary

--- neo_auth/settings.py
+++ neo_auth/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import datetime
 import os
-import cloudinary
 import cloudinary_storage
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
