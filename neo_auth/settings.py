66841db161279e0dacc59def3e0edea6fcbd1220
dostik <dastanforneobis@gmail.com>
2024-03-28 00:24:24 +0600

    add import

--- neo_auth/settings.py
+++ neo_auth/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import datetime
 import os
 import cloudinary_storage
+import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
